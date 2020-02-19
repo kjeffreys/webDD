5b1a7f871d75bf0f8413f6fb814b0343275028e9
kjeffreys <kajeffr1@asu.edu>
2020-02-18 22:35:07 -0500

    Moving it along

--- js/activity2.js
+++ js/activity2.js
@@ -1,12 +1,7 @@
 function checkUsername() {                             
-    var greeting = document.getElementById('greeting');     
-    var userName = document.getElementById('username');
-    if (userName.value.length < 5) {                   
-      var newName = document.createElement('span');
-      newName.entry = userName;
-      greeting.appendChild(newName);
-      newName.innerHTML = userName.innerText;
-    } else {                                              
-      greeting.innerText = 'Hi there' + userName;                             
-    }
+    var msg = document.getElementById('greeting');     
+    var userName = document.getElementById('username');                  
+
+    greeting.textContent = 'Hello there, ';
+
   }
