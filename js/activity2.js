0066d7873e2543f1e51a6548d51609c5f3223667
kjeffreys <kajeffr1@asu.edu>
2020-02-18 22:14:33 -0500

    Work please please

--- js/activity2.js
+++ js/activity2.js
@@ -2,8 +2,8 @@ function checkUsername() {
     var greeting = document.getElementById('greeting');     
     var userName = document.getElementById('username');
     if (userName.value.length < 5) {                   
-      greeting.textContent = 'Hi there' + userName; 
+      greeting.innerText = 'Hi there' + userName; 
     } else {                                              
-      greeting.textContent = 'Hi there' + userName;                             
+      greeting.innerText = 'Hi there' + userName;                             
     }
   }
