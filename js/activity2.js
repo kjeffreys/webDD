f903c27fa9438f6c93532be5655888f6fb14e1b8
kjeffreys <kajeffr1@asu.edu>
2020-02-18 22:45:42 -0500

    Fixed var naming error of msg in js

--- js/activity2.js
+++ js/activity2.js
@@ -2,6 +2,6 @@ function checkUsername() {
     var msg = document.getElementById('greeting');     
     var userName = document.getElementById('username');                  
 
-    greeting.textContent = 'Hello there, ';
+    msg.textContent = 'Hello there, ';
 
   }
