ebd1ecd78ee67bcd50b7486dda59c3f6966ccb54
kjeffreys <kajeffr1@asu.edu>
2020-02-18 23:11:25 -0500

    new msg

--- js/activity2.js
+++ js/activity2.js
@@ -5,7 +5,7 @@ var newName = document.getElementById('username');
 function checkUsername() {                             
 
     var newGreeting = document.createElement('li');
-    var newText = document.createTextNode('Hi ' + newName.textContent);
+    var newText = document.createTextNode('Hi ');
     newGreeting.appendChild(newText);
     list.appendChild(newGreeting);
 
